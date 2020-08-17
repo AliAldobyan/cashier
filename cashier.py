8936064fe43435cb85ed0ba076780a8f6b301cf5
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-08-17 13:02:28 +0300

    cashier coded is added

--- cashier.py
+++ cashier.py
@@ -1,5 +1,31 @@
 def main():
-	# write code here
+
+	list = []
+
+	
+
+	while True:
+		user_input = input("Item (enter \"done\" when finished): ")
+		if user_input == "done":
+			break
+
+		price = input("Price: ")
+		quantity = input("Quantity: ")
+		list.append({"item":user_input, "price":price, "quantity":quantity})
+
+
+	print("\n---------------------\nreceipt\n---------------------\n")
+	total = float(0)
+
+	for i in list:
+		print("%d %s %.3f SAR" % (i.get('quantity'), i.get('item'), i.get('price')))
+		total += (i.get('price') * i.get('quantity'))
+
+	print("---------------------")
+	print("Total Price: %.3f SAR" % float((total)))
+
+
+
 
 if __name__ == '__main__':
 	main()
